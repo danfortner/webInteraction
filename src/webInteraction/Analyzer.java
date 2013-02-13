fbd82d4b034dd75cbfc5fc8e3d183a9fd427dd01
Dan Fortner <daniel.fortner@sv.cmu.edu>
2013-02-12 16:30:00 -0800

    optimized analyzer

--- src/webInteraction/Analyzer.java
+++ src/webInteraction/Analyzer.java
@@ -1,5 +1,6 @@
 package webInteraction;
-import java.util.LinkedList;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 //Analyzer is an algorithm to extract data from text
 public class Analyzer{
@@ -12,10 +13,9 @@ public class Analyzer{
 	
 	//returns analysis of the input data
 	//this algorithm is a work in progress, but currently it:
-	//prints all words in the data without repeats
-	//to-do - remove symbols and punctuation marks
+	//sorts and prints all words without duplicates
 	public String tagWords(){
-		LinkedList<String> wordPile=new LinkedList<String>();
+		TreeSet<String> wordPile=new TreeSet<String>();
 		String[] tempStringArray;
 		
 		input=input.toLowerCase().trim();
@@ -25,7 +25,45 @@ public class Analyzer{
 				wordPile.add(tempStringArray[i]);
 			}
 		}
-		return "Unique, non-repeated words:"+" "+wordPile;
+		wordPile=removeAllPunctuation(wordPile);
+		return "Unique, non-repeated tag words:"+" "+wordPile;
+	}
+
+	//takes an TreeSet and removes all unwanted punctuation
+	//for efficiency an iterator is used and add and remove functions
+	//in a TreeSet are only O(log n)
+	public TreeSet<String> removeAllPunctuation(TreeSet<String> anEntry){
+		String cleanWord;
+		String originalWord;
+		TreeSet<String> cleanWords=new TreeSet<String>(anEntry);
+		Iterator<String> wordIterator = anEntry.iterator();
+		while (wordIterator.hasNext())
+		{
+			originalWord=wordIterator.next();
+			cleanWord=removePunctuation(originalWord);
+			if (!cleanWord.equals(originalWord)){
+				cleanWords.remove(originalWord);
+				if (cleanWord.length()>0)
+					cleanWords.add(cleanWord);
+			}
+		}
+		return cleanWords;
+	}
+
+	//takes a single string and removes unwanted punctuation
+	public String removePunctuation(String anEntry){
+		StringBuffer sb=new StringBuffer();
+		for (int i=0;i<anEntry.length();i++){
+			if ((anEntry.charAt(i) >= 64 && anEntry.charAt(i) <= 90) 	//UPPERCASE and @
+				|| (anEntry.charAt(i) >= 97 &&anEntry.charAt(i) <= 122)	//lowercase
+				|| (anEntry.charAt(i) >= 48 &&anEntry.charAt(i) <=57)	//numbers
+				|| (anEntry.charAt(i)==95)  || (anEntry.charAt(i)==94)	//_ and ^
+				)
+			{
+				sb = sb.append(anEntry.charAt(i));
+			}
+		}
+		return sb.toString();
 	}
 	
 }//end class
