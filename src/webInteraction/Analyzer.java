b83bf3c2932d265fe52518283a3f53782bb668d5
Dan Fortner <daniel.fortner@sv.cmu.edu>
2013-02-18 22:09:28 -0800

    modulatized

--- src/webInteraction/Analyzer.java
+++ src/webInteraction/Analyzer.java
@@ -1,5 +1,4 @@
 package webInteraction;
-import java.util.Iterator;
 import java.util.TreeSet;
 
 //Analyzer is an algorithm to extract data from text
@@ -14,41 +13,23 @@ public class Analyzer{
 	}
 	
 	//returns analysis of the input data, sorts and prints all words without 
-	//duplicates or punctuation.  Uses a TreeSet for the efficiency of a tree.
+	//duplicates or punctuation.  Uses a TreeSet for the efficiency of a tree;
+	//add and remove functions are only O(log n)
 	public String tagWords(){
 		TreeSet<String> wordPile=new TreeSet<String>();
 		String[] stringArray;
-		input=input.toLowerCase().trim();
+		String uniqueWord;
+		input=input.toLowerCase();
 		stringArray=input.split(" ");
-		for (int i=0;i<stringArray.length;i++){ //remove the duplicates
-			if(!wordPile.contains(stringArray[i])){
-				wordPile.add(stringArray[i]);
+		for (int i=0;i<stringArray.length;i++){ 
+			if(!wordPile.contains(stringArray[i])){ //remove the duplicates
+				uniqueWord=removePunctuation(stringArray[i]); //remove the punctuation
+				if (uniqueWord.length()>0) //removes words that were made of punctuation only
+					wordPile.add(uniqueWord);
 			}
 		}
-		wordPile=removeAllPunctuation(wordPile);
 		System.out.println("Unique, non-repeated tag words:");
 		return wordPile.toString();
-	}
-
-	//takes an TreeSet and removes all unwanted punctuation
-	//for efficiency an iterator is used and add and remove functions
-	//in a TreeSet are only O(log n)
-	public TreeSet<String> removeAllPunctuation(TreeSet<String> anEntry){
-		String cleanWord;
-		String originalWord;
-		TreeSet<String> cleanWords=new TreeSet<String>(anEntry);
-		Iterator<String> wordIterator = anEntry.iterator();
-		while (wordIterator.hasNext())
-		{
-			originalWord=wordIterator.next();
-			cleanWord=removePunctuation(originalWord);
-			if (!cleanWord.equals(originalWord)){ //update words that had punctuation removed
-				cleanWords.remove(originalWord);
-				if (cleanWord.length()>0) //removes words that were made of punctuation only
-					cleanWords.add(cleanWord);
-			}
-		}
-		return cleanWords;
 	}
 
 	//takes a single string and removes unwanted punctuation
