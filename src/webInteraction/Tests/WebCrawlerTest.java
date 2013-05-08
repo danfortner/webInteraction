41173d2106acc403ae389be8ee7e6fefb73b3bee
Dan Fortner <daniel.fortner@sv.cmu.edu>
2013-05-08 14:24:19 -0700

    refactor for simplicity

--- src/webInteraction/Tests/WebCrawlerTest.java
+++ src/webInteraction/Tests/WebCrawlerTest.java
@@ -5,39 +5,52 @@ import junit.framework.TestCase;
 public class WebCrawlerTest extends TestCase{
 	
 	//before do
-	WebCrawler test=new WebCrawler("http://www.postimage.org");
+	WebCrawler test=new WebCrawler("http://www.engadget.com/");
 	
+	
+	public void testGoogLocalUrl(){
+		assertTrue(test.isLocalUrl("engadget.com"));
+	}
+	
+	public void testBadLocalUrl(){
+		assertFalse(test.isLocalUrl("yahoo.com"));
+	}
+	
+	public void testFindBaseUrl(){
+		assertEquals("engadget", test.findBaseUrl());
+	}
 	/*
 	 * Only one test may be enabled at a time due to too many web site
 	 * connections being made simultaneously.
 	 */
 	
-	//test web site connection
 //	public void testConnection(){
 //		test.crawl();
 //		assertTrue(test.getContent()!=null);
 //	}
 	
-//	public void testNumberOfLinks(){
-//		test.crawl();
-//		assertEquals(67,test.size());
-//	}
+	public void testNumberOfLinks(){
+		test.crawl();
+		assertEquals(107,test.size());
+	}
 	
 //	public void testLinkDataVisually(){
 //		test.crawl();
 //		//System.out.println(test.getUrlList());
 //	}
 	
-	public void testRemovedTags(){
-	test.crawl();
-	String badUrl="javascript:void(0)";
-	assertFalse(test.getUrlList().contains(badUrl));
-}
+//	public void testRemovedTags(){
+//		test.crawl();
+//		String badUrl="javascript:void(0)";
+//		assertFalse(test.getUrlList().contains(badUrl));
+//	}
 	
 //	public void testSiteLocallity(){
 //		test.crawl();
 //		String testUrl="http://www.googleadservices.com";
 //		assertFalse(test.getUrlList().contains(testUrl));
 //	}
+	
+	
 
 }//end class
--- src/webInteraction/WebCrawler.java
+++ src/webInteraction/WebCrawler.java
@@ -1,6 +1,5 @@
 package webInteraction;
 
-import java.util.Iterator;
 import java.util.TreeSet;
 import org.jsoup.Jsoup;
 
@@ -8,12 +7,13 @@ public class WebCrawler {
 	
 	private String userUrl;
 	private TreeSet <String> urlList;
-	private int size;
 	private final int MAXBUFFER = 200;
+	private String baseUrl;
 	
 	public WebCrawler(String urlEntry){
 		userUrl=urlEntry;
 		urlList=new TreeSet<String>();
+		baseUrl=findBaseUrl();
 	}
 	
 	public TreeSet <String> getUrlList(){
@@ -24,31 +24,6 @@ public class WebCrawler {
 		return urlList.size();
 	}
 	
-	public void crawl(){
-		Boolean end=false;
-		int sizeChange;
-		parseAllLinks(getContent(userUrl));
-		//iterator setup
-		TreeSet<String> toCompare=new TreeSet<String>();
-		toCompare.addAll(urlList);
-		Iterator<String> urlIterator=toCompare.iterator();
-		while (!end){
-			size=size();
-			while (urlIterator.hasNext()){
-				String nextPage=getContent(urlIterator.next());
-				parseAllLinks(nextPage);
-			}//end iterator while
-			sizeChange=size();
-			if (sizeChange==size||size>MAXBUFFER){
-				end=true;
-			}
-			//reset to check for changes
-			toCompare.clear();
-			toCompare.addAll(urlList);
-			urlIterator=toCompare.iterator();
-		}//end if no more changes were found
-	}//end crawl
-
 	public String getContent(){
 		return getContent(userUrl);
 	}
@@ -59,25 +34,56 @@ public class WebCrawler {
 		return web.getData();
 	}
 	
-	private TreeSet <String> parseAllLinks(String content){
+	public void crawl(){
+		TreeSet<String> toBeCrawled=new TreeSet<String>();
+		//initially fill the to-do list using the base url
+		toBeCrawled.addAll(findNewLinks(getContent(userUrl)));
+		//continue making a pile of new links & crawling them
+		while (toBeCrawled.size()!=0){
+			String element=toBeCrawled.first();
+			toBeCrawled.addAll(findNewLinks(element));
+			urlList.add(element);
+			toBeCrawled.remove(element);
+			if (toBeCrawled.size()>MAXBUFFER){
+				break;
+			}
+		}//end while
+	}//end crawl
+	
+	private TreeSet <String> findNewLinks(String content){
+		TreeSet <String> newLinks = new TreeSet<String>();
 		org.jsoup.nodes.Document document = Jsoup.parse(content);
+		//for every piece of the document, search for links
 		for (org.jsoup.nodes.Element element: document.getElementsByTag("a")){
-			if (isCleanUrl(element.attr("href"))
-					&&(!urlList.contains(element.attr("href")))){
-				urlList.add(element.attr("href"));
+			//if it is a local Url && is unique - add to the list
+			if (isLocalUrl(element.attr("href"))&&!urlList.contains(element.attr("href"))){
+				newLinks.add(element.attr("href"));
 				System.out.println(element.attr("href"));
 			}
 		}
-		return urlList;
+		return newLinks;
 	}
 	
-	private Boolean isCleanUrl(String urlEntry){
-		if (urlEntry.startsWith("http://")) return true;
-		if (urlEntry.startsWith("http://www.")) return true;
-		if (urlEntry.startsWith("https://")) return true;
-		if (urlEntry.startsWith("https://www.")) return true;
-		return false;
-	}	
+	//finds the base for the domain name
+	public String findBaseUrl(){
+		String urlBase=userUrl;
+		int firstDot, secondDot = 0;
+		int slashPosition=0;
+		firstDot=urlBase.indexOf('.');
+		if(firstDot<12){//contains 'www'
+			//System.out.println(firstDot+" "+urlBase+" "+secondDot);
+			secondDot=urlBase.indexOf(".", 12);
+			urlBase=(String) urlBase.subSequence(firstDot+1, secondDot);
+		} else { //does not contain 'www'
+			slashPosition=urlBase.indexOf('/');
+			urlBase=(String) urlBase.subSequence(slashPosition+2, firstDot);
+		}
+		return urlBase;
+	}
+	
+	public Boolean isLocalUrl(String urlEntry){
+		return urlEntry.contains(baseUrl);
+	}
 	
 	
 }//end class
